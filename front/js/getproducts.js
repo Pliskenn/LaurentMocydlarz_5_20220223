56e34c8f02b72681aea28cf90f11f6af1f4667a3
Pliskenn <laurent@mocydlarz.me>
2022-03-28 20:56:07 +0200

    Ajout InnerHTML

--- front/js/getproducts.js
+++ front/js/getproducts.js
@@ -1,15 +1,18 @@
-async function getAllProducts() {
-  return await fetch("http://localhost:3000/api/products")
+function getAllProducts() {
+  fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => data);
-}
-async function displayProduct() {
-  const products = await getAllProducts();
-  console.log(products);
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        document.getElementById("items").innerHTML+=`<a href="./product.html?id=${data[i]._id}">
+        <article>
+          <img src="${data[i].imageUrl}" alt="${data[i].altTxt}">
+          <h3 class="productName">${data[i].name}</h3>
+          <p class="productDescription">${data[i].description}</p>
+        </article>
+      </a>`
+      }
+    });
 }
 
-displayProduct();
+getAllProducts();
 
-for (var products = 0; products < products.lenght; products++) {
-  console.log(products[i]);
-}
